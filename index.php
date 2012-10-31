2b59dad9b2ae51b79c9f027117fbae1a72d50ba0
crebstar <crebstar.paul@yahoo.com>
2012-10-30 23:53:14 -0500

    Gradient And Headers Added

--- index.php
+++ index.php
@@ -9,13 +9,30 @@ This is our welcome page. The first page users will see
         <Link href="./css/skeleton.css" rel="stylesheet" type="text/css">
         <title> Snapshot - The Place For Amateur Photographers </title>
     </head>
-    <body>
+    <body >
         
+        <div id="wrapper"> 
+            <div id="header"> 
+                <div class="topBlock">
+                    <div class="topHolder">
+                    
+                        
+                        
+                        
+                    <!-- end topHolder -->
+                    </div>
+                <!-- end topBlock -->    
+                </div>
+                
+            <!-- end header -->
+            </div>
+        <!-- end wrapper -->
+        </div>
         
         
         <?php
                 // Insert php code here
-                echo "<h3> Snapshot </h3>";
+                
         ?>
     </body>
 </html>
